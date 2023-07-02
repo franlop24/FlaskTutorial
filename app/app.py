b8b8802adcfeb821b7b4e445b0131ae1e6a6e611
franlops <franlopbri@gmail.com>
2023-07-01 23:34:54 -0600

    04 How to Use Web Forms in Flask

--- app/app.py
+++ app/app.py
@@ -1,37 +1,32 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b7c08d8c7423a6ae2529ab67441cf3ed7f9048cfedfb1fe0'
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html')
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html')
+messages = [{
+    'title': 'Message One', 'content': 'Message One Content'
+}, {
+    'title': 'Message two', 'content': 'Message Two Content'
+}]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
-@app.route('/messages/<int:idx>/')
-def message(idx):
-    app.logger.info('Building the messages list')
-    messages = ['Message zero', 'Message one', 'Message two']
-    try:
-        app.logger.debug('Get message with index {}'.format(idx))
-        return render_template('message.html', message=messages[idx])
-    except Exception:
-        app.logger.debug('Index {} is causing an IndexError'.format(idx))
-        abort(404)
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('index'))
 
-@app.route('/contact/')
-def contact():
-    return '<h1>Contact Page</h1>'
-
-@app.route('/500/')
-def error500():
-    abort(500)
+    return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
